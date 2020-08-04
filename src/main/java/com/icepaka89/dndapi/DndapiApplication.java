792de54ea272f520c73420fc99ba16d7ee56c1ef
icepaka89 <alphapaka89@gmail.com>
2020-08-04 15:45:00 -0400

    initial commit

---
+++ src/main/java/com/icepaka89/dndapi/DndapiApplication.java
@@ -0,0 +1,13 @@
+package com.icepaka89.dndapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icepaka89/dndapi/DndapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icepaka89.dndapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
